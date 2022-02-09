9c9bfe7f23780d96f034819ec7fb813c2dba1a21
Alejandro Toba <alejandro.toba@payco.co>
2022-02-09 12:25:36 -0400

    catch errors

--- api/src/index.ts
+++ api/src/index.ts
@@ -21,11 +21,14 @@ app.get('/', (req: Request, res: Response) => {
 })
 
 app.get('/btc', (req: Request, res: Response) => {    
-    request('https://api.coinbase.com/v2/prices/BTC-USD/spot?date='+req.query.date, 
+    var result = request('https://api.coinbase.com/v2/prices/BTC-USD/spot?date='+req.query.date, 
     function (error: any, response: any, body: any) {  
-        if(error) res.send({error: error, message: 'Error'});
+        if(error) res.send({error: error, message: 'Error api coinbase...'});
         res.send(body);
     });
+    result.on('error', function(error:any){
+        res.send({error: error, message: 'Error in request'});
+    })
 })
 
 app.listen(port, function () {
